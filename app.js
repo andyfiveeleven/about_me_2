42950f8b07672a9332b81f1d0838b0e6e612b061
ohjonah <ohjonah@gmail.com>
2017-05-10 20:06:42 -0700

    added while loop i.e. game one. shortened code

--- app.js
+++ app.js
@@ -1,92 +1,98 @@
 'use strict';
 
-var greeting = prompt('Let\'s see if our names have letters in common! Ready? (Y/N)').toUpperCase();
+var userName = prompt('Hi! What\'s your name?');
 
-if (greeting === 'Y' || greeting === 'YA' || greeting === 'YAH' || greeting === 'YES') {
+var defaultAnswersArr = ['Y', 'YA', 'YAH', 'YES', 'N', 'NA', 'NAH', 'NO'];
 
-  var sum = 0;
+var playGameOneQuestion = prompt('Oh, you want to know my name? How about we play a game? Ready? (Y/N)').toUpperCase();
 
-  var questionJ = prompt('Do you have a "J" in your name?').toUpperCase();
-  console.log('user has J:', questionJ);
-  var questionO = prompt('Do you have a "O" in your name?').toUpperCase();
-  console.log('user has O:', questionO);
-  var questionN = prompt('Do you have a "N" in your name?').toUpperCase();
-  console.log('user has N:', questionN);
-  var questionA = prompt('Do you have a "A" in your name?').toUpperCase();
-  console.log('user has A:', questionA);
-  var questionH = prompt('Do you have a "H" in your name?').toUpperCase();
-  console.log('user has H:', questionH);
+// GAME ONE
 
-  var questionArr = [];
+if (defaultAnswersArr.indexOf(playGameOneQuestion) <= 3) {
+  console.log('YES - position of users answers in defaultAnswersArr: ', defaultAnswersArr.indexOf(playGameOneQuestion));
 
-  if (questionJ === 'Y' || questionJ === 'YA' || questionJ === 'YAH' || questionJ === 'YES') {
-    questionArr.unshift(1);
-  } else {
-    questionArr.unshift(0);
+  var number;
+  var gameOneCounter = 0;
+  var gameOneAntiCounter = 4;
+
+  while (number !== 5 && gameOneCounter !== 4) { // while number is not 5, run some code. if gameOneAntiCounter = 0, break
+    number = parseInt(prompt('How many letters are in my first name?'));
+
+    if (number < 5) {
+      gameOneAntiCounter--;
+      gameOneCounter++;
+      alert('My name is longer than that! You have ' + gameOneAntiCounter + ' tries left!');
+      console.log('number of attempts:', gameOneCounter);
+      console.log('user number:', number);
+
+    } else if (number > 5) {
+      gameOneAntiCounter--;
+      gameOneCounter++;
+      alert('My name is shorter than that!  You have ' + gameOneAntiCounter + ' tries left!');
+      console.log('number of attempts:', gameOneCounter);
+      console.log('user number:', number);
+
+    } else if (number === NaN || number === null) {
+      gameOneAntiCounter--;
+      gameOneCounter++;
+      alert('Please enter a number! Counts as a try!' + gameOneAntiCounter + ' tries left!');
+      console.log('number of attempts:', gameOneCounter);
+      console.log('user number:', number);
+    }
   }
 
-  if (questionO === 'Y' || questionO === 'YA' || questionO === 'YAH' || questionO === 'YES') {
-    questionArr.unshift(1);
-  } else {
-    questionArr.unshift(0);
+  if (number === 5) {
+      alert('You win! I have 5 letters in my name.')
+    } else if (gameOneCounter === 4) {
+      alert('You lose! Oh well! Let\'s play another game');
   }
 
-  if (questionN === 'Y' || questionN === 'YA' || questionN === 'YAH' || questionN === 'YES') {
-    questionArr.unshift(1);
-  } else {
-    questionArr.unshift(0);
-  }
+  // game two
 
-  if (questionA === 'Y' || questionA === 'YA' || questionA === 'YAH' || questionA === 'YES') {
-    questionArr.unshift(1);
-  } else {
-    questionArr.unshift(0);
-  }
+  var greeting = prompt('Let\'s see if our names have letters in common! Ready? (Y/N)').toUpperCase();
 
-  if (questionH === 'Y' || questionH === 'YA' || questionH === 'YAH' || questionH === 'YES') {
-    questionArr.unshift(1);
-  } else {
-    questionArr.unshift(0);
-  }
+  if (defaultAnswersArr.indexOf(greeting) <= 3) {
 
-  console.log('user response in array format:', questionArr);
-  console.log('user response in code form:', questionArr.join(''));
+    var lettersArr = ['J', 'O', 'N', 'A', 'H'];
+    var questionAnswersArr = [];
+    var userLetterArr = [];
 
-  function sumMachine() {
-    for(var i = 0; i < questionArr.length; i++) {
-      sum += questionArr[i];
+    for (var i = 0; i < lettersArr.length; i++) {
+      var letterQuestionYesNo = prompt('Do you have a "' + lettersArr[i] + '" in your first name?').toUpperCase();
+      console.log('user has "' + lettersArr[i] + '" in their first name? ' + letterQuestionYesNo);
+
+      if (defaultAnswersArr.indexOf(letterQuestionYesNo) <= 3) {
+        questionAnswersArr.push(1);
+        userLetterArr.push(lettersArr[i]);
+      } else {
+        questionAnswersArr.push(0);
+      }
     }
 
-    // in-scope variable
-    console.log('in scope variable:', sum);
+    console.log('user response in an array: ' + questionAnswersArr + '. user response joined: ' + questionAnswersArr.join('') + '.');
 
-    if (sum === 5) {
-      alert('We have 5 letters in common!');
-    } else if (sum === 4) {
-      alert('We have 4 letters in common!');
-    } else if (sum === 3) {
-      alert('We have 3 letters in common!');
-    } else if (sum === 2) {
-      alert('We have 2 letters in common!');
-    } else if (sum === 1) {
-      alert('We have 1 letters in common!');
-    } else if (sum === 0) {
-      alert('We have nothing in common!');
-    };
-  }
+    var sum = 0;
 
-  // global variable sum
-  console.log('out of scope variable:', sum);
+    for (var i = 0; i < questionAnswersArr.length; i++) {
+      sum += questionAnswersArr[i];
+    }
+    console.log('total sum:', sum);
 
-  sumMachine();
+    alert('We have ' + sum + ' letters in common!');
 
-} else if (greeting === 'N' || greeting === 'NA' || greeting === 'NAH' || greeting === 'NO') {
 
-  var greetingQuestionTwo = prompt('Do you like having fun? (Y/N)').toUpperCase();
+  } else if (defaultAnswersArr.indexOf(greeting) > 3) {
+    var greetingQuestionTwo = prompt('Do you like having fun? (Y/N)').toUpperCase();
 
-    if ((greeting === 'N' || greeting === 'NA' || greeting === 'NAH' || greeting === 'NO') && (greetingQuestionTwo === 'N' || greetingQuestionTwo === 'NA' || greetingQuestionTwo === 'NAH' || greetingQuestionTwo === 'NO')) {
+    if ((defaultAnswersArr.indexOf(greeting) > 3) && (defaultAnswersArr.indexOf(greetingQuestionTwo) > 3)) {
       alert('Two NO\'s in a row?? You\'re no fun!');
     } else {
       alert('Well, we can play a different game later!');
     }
+  } else {
+    alert('Please put a valid response!');
+  }
+
+} else {
+  console.log('NO - position of users answers in defaultAnswersArr: ', defaultAnswersArr.indexOf(playGameOneQuestion));
 }
